9bdfc5295aaf197779805211529b0cd4943b974b
Colin Bellino <bellino.colin@gmail.com>
2019-11-25 00:25:04 +0100

    ♻ Convert legacy 1 to new tileset

--- Assets/Game/Scripts/GameSettings.cs
+++ Assets/Game/Scripts/GameSettings.cs
@@ -6,7 +6,6 @@ using UnityEngine.AddressableAssets;
 [CreateAssetMenu(fileName = "Settings", menuName = "LD43/Settings", order = 1)]
 public class GameSettings : ScriptableObject
 {
-	public List<GameObject> levels = new List<GameObject>(); // TODO: Remove me
 	public List<AssetReference> Levels = new List<AssetReference>();
 	public List<SacrificeData> sacrifices = new List<SacrificeData>();
 	public List<GameObject> enemyPrefabs = new List<GameObject>();
